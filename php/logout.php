fe03e54d30f5e8833732b0e7c93f7c67efde529b
yosoycalde <juancalderon2005.18@gmail.com>
2025-10-06 16:04:04 -0500

    actualizado

--- php/logout.php
+++ php/logout.php
@@ -1,19 +1,14 @@
 <?php
-// logout.php - Cierra la sesión del administrador
 session_start();
 
-// Destruir todas las variables de sesión
 $_SESSION = array();
 
-// Destruir la cookie de sesión
 if (isset($_COOKIE[session_name()])) {
     setcookie(session_name(), '', time()-3600, '/');
 }
 
-// Destruir la sesión
 session_destroy();
 
-// Redirigir al login
 header('Location: login.php');
 exit;
 ?>
